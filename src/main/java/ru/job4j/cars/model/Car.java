8d5e0a9fad38c8bef99b3f82fe8e86e3b2cb72f6
aleksandrblagov <blagov13rus@gmail.com>
2024-06-08 18:08:22 +0300

    6. Модели и связи. Машины и владельцы [#4744 #499567]

--- src/main/java/ru/job4j/cars/model/Car.java
+++ src/main/java/ru/job4j/cars/model/Car.java
@@ -24,12 +24,8 @@ public class Car {
     @JoinColumn(name = "engine_id", foreignKey = @ForeignKey(name = "ENGINE_ID_FK"))
     private Engine engine;
 
-    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinTable(name = "history_owner", joinColumns = {
-            @JoinColumn(name = "car_id", nullable = false, updatable = false)},
-            inverseJoinColumns = {
-                    @JoinColumn(name = "owner_id", nullable = false, updatable = false)})
-    private Set<Owner> owners = new HashSet<>();
+    @OneToMany(mappedBy = "car", cascade = CascadeType.ALL)
+    private Set<Owner> carOwners = new HashSet<>();
 
     @OneToMany(mappedBy = "car", cascade = CascadeType.ALL)
     private Set<File> files = new HashSet<>();
---
+++ src/main/java/ru/job4j/cars/model/CarOwner.java
@@ -0,0 +1,29 @@
+package ru.job4j.cars.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "car_owner")
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class CarOwner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "car_id")
+    private Car car;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
+}
